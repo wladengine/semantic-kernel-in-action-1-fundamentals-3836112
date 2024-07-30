a1a9935a6079e8bb410b85adc885841c1c23f74d
wladengine <wladengine@yandex.ru>
2024-07-30 18:47:07 +0000

    task 04_05

--- src/04_05b/Program.cs
+++ src/04_05b/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿﻿using _04_05b;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+await HandlebarsPlannerPractice.Execute();
