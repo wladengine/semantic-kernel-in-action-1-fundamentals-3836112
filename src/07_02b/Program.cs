26f6537281f8d9b428637778a80d9544f3db281d
wladengine <wladengine@yandex.ru>
2024-07-31 13:53:58 +0000

    task 07 part 1

--- src/07_02b/Program.cs
+++ src/07_02b/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using _07_02b;
+
 Console.WriteLine("Hello, World!");
+
+var researcher = new ResearcherProject();
+await researcher.ExecuteAsync();
